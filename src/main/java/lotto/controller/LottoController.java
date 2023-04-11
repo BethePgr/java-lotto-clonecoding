ba755a8c7cba5659a9f291ba563a44adffc8aa5e
jongwonpc <c3392302@naver.com>
2023-04-12 01:40:59 +0900

    로또 결과 기능 구현 완료, controller 구현완료

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,11 @@
 package lotto.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import lotto.domain.LottoResult;
 import lotto.domain.LottoTicket;
 import lotto.view.InputView;
 import lotto.view.OutputView;
@@ -9,10 +13,13 @@ import lotto.view.OutputView;
 public class LottoController {
 
     InputView inputView = new InputView();
-
+    List<List<Integer>> lottoTickets;
+    List<Integer> winningNumbers;
+    int bonusNumber;
     public void run(){
         chargeLottoPurchaseAmount();
         receiveWinningNumbers();
+        receiveBonusNumber();
     }
 
     public void chargeLottoPurchaseAmount() {
@@ -22,7 +29,7 @@ public class LottoController {
         int userMoney = Integer.parseInt(userInput);
         int ticketNumber = userMoney / 1000;
         LottoTicket lottoTicket = new LottoTicket(ticketNumber);
-        List<List<Integer>> lottoTickets = lottoTicket.createLottoTickets();
+        lottoTickets = lottoTicket.createLottoTickets();
         OutputView.printLottoNumber(ticketNumber);
         OutputView.printLottoTickets(lottoTickets);
     }
@@ -30,12 +37,19 @@ public class LottoController {
     public void receiveWinningNumbers(){
         String userInput = inputView.inputWinningNumber();
         inputView.validWinningNumber(userInput);
+        winningNumbers = Arrays.stream(userInput.split(",")).map(num -> Integer.parseInt(num)).collect(
+            Collectors.toList());
     }
 
     public void receiveBonusNumber(){
         String userInput = inputView.inputBonusNumber();
         inputView.validBonusNumber(userInput);
+        bonusNumber = Integer.parseInt(userInput);
     }
 
+    public void winningNumberCount(){
+        LottoResult lottoResult = new LottoResult(lottoTickets,winningNumbers,bonusNumber);
+        HashMap<String,Integer> prizeCount = lottoResult.countWin();
+    }
 
 }
