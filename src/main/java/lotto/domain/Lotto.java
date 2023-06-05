e45b1b9b6b1d200bf89cca1b474b0952a5d171bd
jongwonpc <c3392302@naver.com>
2023-06-06 06:25:44 +0900

    RESTART

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -9,32 +9,9 @@ public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateSize(numbers);
-        isDuplicate(numbers);
-        validateNumber(numbers);
-        Collections.sort(numbers);
+
         this.numbers = numbers;
     }
 
-    private void validateSize(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
-    }
 
-    private void isDuplicate(List<Integer> numbers){
-        if(numbers.stream().distinct().count() != 6){
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있으면 안 됩니다.");
-        }
-    }
-
-    private void validateNumber(List<Integer> numbers){
-        if(numbers.stream().filter(num -> num>=1 && num <=45).count() != 6){
-            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자는 1에서 45 사이여야 합니다.");
-        }
-    }
-
-    public List<Integer> getNumbers(){
-        return Collections.unmodifiableList(numbers);
-    }
 }
