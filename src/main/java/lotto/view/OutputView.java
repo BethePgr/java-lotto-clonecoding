cfcbd2b9bd14146533852b65dd0bd5d508fcef60
SEC <c3392302@naver.com>
2023-06-14 18:04:08 +0900

    feat(lottoGenerator) : 구입 금액이 1000원으로 나누어 떨어지는지 검증하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,12 +7,12 @@ public class OutputView {
 
     private static final String HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE = "개를 구매했습니다.";
 
-    public static void printHowManyLottoUserPurchased(int lottoQuantity){
+    public static void printHowManyLottoUserPurchased(int lottoQuantity) {
         System.out.println(lottoQuantity + HOW_MANY_LOTTO_USER_PURCHASED_MESSAGE);
     }
 
-    public static void printLottos(Lottos lottos){
-        for(Lotto lotto : lottos.getLottos()){
+    public static void printLottos(Lottos lottos) {
+        for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.toString());
         }
     }
