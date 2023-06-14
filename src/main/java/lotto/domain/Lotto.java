3c8444553dd96f1cd128052baa5b81d21c14f201
SEC <c3392302@naver.com>
2023-06-14 16:50:18 +0900

    RESTART

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,40 +1,21 @@
 package lotto.domain;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
-        validateSize(numbers);
-        isDuplicate(numbers);
-        validateNumber(numbers);
-        Collections.sort(numbers);
+        validate(numbers);
         this.numbers = numbers;
     }
 
-    private void validateSize(List<Integer> numbers) {
+    private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException();
         }
     }
 
-    private void isDuplicate(List<Integer> numbers){
-        if(numbers.stream().distinct().count() != 6){
-            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있으면 안 됩니다.");
-        }
-    }
-
-    private void validateNumber(List<Integer> numbers){
-        if(numbers.stream().filter(num -> num>=1 && num <=45).count() != 6){
-            throw new IllegalArgumentException("[ERROR] 로또 번호의 숫자는 1에서 45 사이여야 합니다.");
-        }
-    }
-
-    public List<Integer> getNumbers(){
-        return Collections.unmodifiableList(numbers);
-    }
+    // TODO: 추가 기능 구현
 }
