7f0e076679fc3f5677904b3784beccb4e35842b2
SEC <c3392302@naver.com>
2023-07-13 21:01:49 +0900

    feat(lottoGenerator) : 구입 금액이 1000원으로 나누어 떨어지는지 검증하는 기능 구현

--- src/main/java/lotto/domain/WinningStatistics.java
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class WinningStatistics {
 
-    public static int compareNumbersWithWinningNumbers(Lotto lotto, WinningLotto winningLotto){
+    public static int compareNumbersWithWinningNumbers(Lotto lotto, WinningLotto winningLotto) {
         List<Integer> numbers = lotto.getNumbers();
         List<Integer> winningNumbers = winningLotto.getWinningNumbers();
         return (int) numbers.stream().filter(winningNumbers::contains).count();
     }
 
-    public static boolean compareNumbersWithBonusNumber(Lotto lotto, WinningLotto winningLotto){
+    public static boolean compareNumbersWithBonusNumber(Lotto lotto, WinningLotto winningLotto) {
         List<Integer> numbers = lotto.getNumbers();
         int bonusNumber = winningLotto.getBonusNumber();
         return numbers.contains(bonusNumber);
